36df139eb72c0de56524fcb66516cd86902a38ac
Aditya Agarwal <aditya.7@outlook.com>
2021-03-07 01:47:34 +0530

    Designed the welcome page

---
+++ welcome.php
@@ -0,0 +1,28 @@
+<?php
+require("index.php") ;
+
+session_start();
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']==false ){
+	header("location: login.php");
+	exit();
+}
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>
+ 		Welcome Page
+ 	</title>
+ </head>
+ <body>
+ 	<div class="container middle text-center">
+ 		<div class="container p-4">
+		 	<h3>You are logged in. Hello <?php echo htmlspecialchars($_SESSION['username']) ?></h3><br><br>
+		 	<div class="text-center">
+			 	<a href="logout.php"><button class="btn btn-primary">Log Out</button></a>
+		 	</div>
+ 		</div>
+ 	</div>
+ </body>
+ </html>
